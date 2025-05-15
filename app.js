3a636b9117196cdd67add9facf868369c2be8b81
Liecie Salvador <liesalrodav@gmail.com>
2025-05-15 12:55:22 -0700

    Adding styles.css

--- app.js
+++ app.js
@@ -13,8 +13,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    // res.sendFile(path.join(__dirname, 'public', 'index.html'));
-    res.sendFile('index.html')
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.post('/gemini', async (req, res) => {
